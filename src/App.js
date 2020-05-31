625a5c6d3387d73326a1c0e476ea3bcb9af76516
Pedro Silva <pedro.rls93@gmail.com>
2020-05-31 15:44:51 -0300

    basic reducer to handle secretword calls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import hookActions from "./actions/hookActions";
 import "./App.css";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setSecredWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
   return <div data-test="app-component" />;
 };
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { shallow } from "enzyme";
-import App from './App';
+import React from "react";
+import { mount } from "enzyme";
+import App from "./App";
 import { findByTestAttr } from "../test/testUtils";
+import hookActions from "./actions/hookActions";
 
+const mockFn = jest.fn();
 
-const setup = () => shallow(<App />);
+const setup = () => {
+  hookActions.getSecretWord = mockFn;
+  mockFn.mockClear();
+  return mount(<App />);
+};
 
-test('App rendes without error', () => {
+test("App rendes without error", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "app-component");
-  expect(component.length).toBe(1)
+  expect(component.length).toBe(1);
+});
+
+describe("Function getSecretWord", () => {
+  test("should gets called on app mount", () => {
+    setup();
+    expect(mockFn).toHaveBeenCalled();
+  });
 });
